ba212e5df8b19aed22be811c5def1ec87f6911b5
steventouba <steventouba1@gmail.com>
2020-11-30 21:56:02 -0800

    S3

--- src/main/java/com/steven/willysbakeshop/configuration/s3/S3Application.java
+++ src/main/java/com/steven/willysbakeshop/configuration/s3/S3Application.java
@@ -3,6 +3,7 @@ package com.steven.willysbakeshop.configuration.s3;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 
+// Todo: complete and test s3 bucket
 public class S3Application {
     private static final AwsCredentials credentials;
     private static String bucketName;
